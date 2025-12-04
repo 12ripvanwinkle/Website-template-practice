38465f2352a37ac7c0da03d5089a61d2f94b59ac
Nitesh <niteshanthony3@gmail.com>
2025-12-03 21:11:39 -0500

    implemented the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      {/* Gradient image */}
+      <img src="src/assets/gradient.png" alt="Gradient-img" className='absolute top-0 right-0 opacity-60 -z-10'/>
+
+      {/* Blur effect */}
+      <div className="h-0 w-160 absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-30 -z-10"></div>
+
+      <Navbar />
+    </main>
   )
 }
 
